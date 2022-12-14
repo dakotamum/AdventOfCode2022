22206bca6f5a4a44891102d493eb06c20ef9765d
Dakota Mumford <dakotamum@gmail.com>
2022-12-13 23:01:23 -0700

    completed day 12

--- day12/day12.py
+++ day12/day12.py
@@ -1,133 +1,72 @@
-import string
+from string import ascii_lowercase
+
 elevation_map = open('input.txt','r').read().strip().split('\n')
 letter_to_number = dict()
-
-letter_to_number['E'] = 27
-for i in string.ascii_lowercase:
-    letter_to_number[i] = string.ascii_lowercase.index(i) + 1
+for i in ascii_lowercase:
+    letter_to_number[i] = ascii_lowercase.index(i)
 letter_to_number['S'] = 0
+letter_to_number['E'] = 25
 
 # part a
 startCoord = []
 for row in range(len(elevation_map)):
     for col in range(len(elevation_map[row])):
-        if letter_to_number[elevation_map[row][col]] == 0:
+        if elevation_map[row][col] == 'S':
             startCoord = [row, col]
-
 shortest_path = []
 already_visited = [startCoord]
 priority_queue = [[startCoord, [startCoord]]]
 
 while(len(priority_queue) > 0):
     curr_node = priority_queue.pop(0)
-    row = curr_node[0][0]
-    col = curr_node[0][1]
+    row = curr_node[0][0]; col = curr_node[0][1]
     curr_history = curr_node[1].copy()
-    if col < len(elevation_map[0]) - 1 and [row, col + 1] not in already_visited and letter_to_number[elevation_map[row][col + 1]] < letter_to_number[elevation_map[row][col]] + 2:
-        curr_history_copy = curr_history.copy()
-        curr_history_copy.append([row, col + 1])
-        priority_queue.append([[row, col + 1], curr_history_copy])
-        already_visited.append([row, col + 1])
-        if elevation_map[row][col + 1] == 'E':
-            shortest_path = curr_history_copy
+    for dr, dc in [[0,1],[1,0],[0,-1],[-1,0]]:
+        r = row + dr; c = col + dc
+        if not (0 <= r < len(elevation_map) and 0 <= c < len(elevation_map[0])):
+            continue
+        if not (letter_to_number[elevation_map[r][c]] <= letter_to_number[elevation_map[row][col]] + 1):
+            continue
+        if [r, c] in already_visited:
+            continue
+        already_visited.append([r,c])
+        new_history = curr_history.copy()
+        new_history.append([r,c])
+        priority_queue.append([[r, c], new_history])
+        if elevation_map[r][c] == 'E':
+            shortest_path = new_history
+            priority_queue.clear()
             break
-    if row < len(elevation_map) - 1    and [row + 1, col] not in already_visited and letter_to_number[elevation_map[row + 1][col]] < letter_to_number[elevation_map[row][col]] + 2:
-        curr_history_copy = curr_history.copy()
-        curr_history_copy.append([row + 1, col])
-        priority_queue.append([[row + 1, col], curr_history_copy])
-        already_visited.append([row + 1, col])
-        if elevation_map[row + 1][col] == 'E':
-            shortest_path = curr_history_copy
-            break
-    if col > 0                         and [row, col - 1] not in already_visited and letter_to_number[elevation_map[row][col - 1]] < letter_to_number[elevation_map[row][col]] + 2:
-        curr_history_copy = curr_history.copy()
-        curr_history_copy.append([row, col - 1])
-        priority_queue.append([[row, col - 1], curr_history_copy])
-        already_visited.append([row, col - 1])
-        if elevation_map[row][col - 1] == 'E':
-            shortest_path = curr_history_copy
-            break
-    if row > 0                         and [row - 1, col] not in already_visited and letter_to_number[elevation_map[row - 1][col]] < letter_to_number[elevation_map[row][col]] + 2:
-        curr_history_copy = curr_history.copy()
-        curr_history_copy.append([row - 1, col])
-        priority_queue.append([[row - 1, col], curr_history_copy])
-        already_visited.append([row - 1, col])
-        if elevation_map[row - 1][col] == 'E':
-            shortest_path = curr_history_copy
-            break
-
-
-# almost working....
-
-# startCoord = []
-# for row in range(len(elevation_map)):
-#     for col in range(len(elevation_map[row])):
-#         if letter_to_number[elevation_map[row][col]] == 27:
-#             startCoord = [startCoord]
-
-# shortest_path = []
-# already_visited = [startCoord]
-# priority_queue = [[startCoord, [startCoord]]]
-
-# while(len(priority_queue) > 0):
-#     curr_node = priority_queue.pop(0)
-#     row = curr_node[0][0]
-#     col = curr_node[0][1]
-#     curr_history = curr_node[1].copy()
-#     if col < len(elevation_map[0]) - 1 and [row, col + 1] not in already_visited and letter_to_number[elevation_map[row][col + 1]] > letter_to_number[elevation_map[row][col]] - 2 and letter_to_number[elevation_map[row][col + 1]] < letter_to_number[elevation_map[row][col]] + 2:
-#         curr_history_copy = curr_history.copy()
-#         curr_history_copy.append([row, col + 1])
-#         priority_queue.append([[row, col + 1], curr_history_copy])
-#         already_visited.append([row, col + 1])
-#         if elevation_map[row][col + 1] == 'a':
-#             shortest_path = curr_history_copy
-#             break
-#     if row < len(elevation_map) - 1    and [row + 1, col] not in already_visited and letter_to_number[elevation_map[row + 1][col]] > letter_to_number[elevation_map[row][col]] - 2 and letter_to_number[elevation_map[row + 1][col]] < letter_to_number[elevation_map[row][col]] + 2:
-#         curr_history_copy = curr_history.copy()
-#         curr_history_copy.append([row + 1, col])
-#         priority_queue.append([[row + 1, col], curr_history_copy])
-#         already_visited.append([row + 1, col])
-#         if elevation_map[row + 1][col] == 'a':
-#             shortest_path = curr_history_copy
-#             break
-#     if col > 0                         and [row, col - 1] not in already_visited and letter_to_number[elevation_map[row][col - 1]] > letter_to_number[elevation_map[row][col]] - 2 and letter_to_number[elevation_map[row][col - 1]] < letter_to_number[elevation_map[row][col]] + 2:
-#         curr_history_copy = curr_history.copy()
-#         curr_history_copy.append([row, col - 1])
-#         priority_queue.append([[row, col - 1], curr_history_copy])
-#         already_visited.append([row, col - 1])
-#         if elevation_map[row][col - 1] == 'a':
-#             shortest_path = curr_history_copy
-#             break
-#     if row > 0                         and [row - 1, col] not in already_visited and letter_to_number[elevation_map[row - 1][col]] > letter_to_number[elevation_map[row][col]] - 2 and letter_to_number[elevation_map[row - 1][col]] < letter_to_number[elevation_map[row][col]] + 2:
-#         curr_history_copy = curr_history.copy()
-#         curr_history_copy.append([row - 1, col])
-#         priority_queue.append([[row - 1, col], curr_history_copy])
-#         already_visited.append([row - 1, col])
-#         if elevation_map[row - 1][col] == 'a':
-#             shortest_path = curr_history_copy
-#             break
-
-
-
-
-printedMap = []
-for i in range(len(elevation_map)):
-    newRow = []
-    for j in range(len(elevation_map[i])):
-        if [i, j] in shortest_path:
-            newRow.append('#')
-        else:
-            newRow.append(elevation_map[i][j])
-    printedMap.append(newRow)
-
-fileOut = open('output.txt', 'w')
-for line in printedMap:
-    strLine = ""
-    for i in line:
-        strLine += i
-    fileOut.write(strLine)
-    fileOut.write('\n')
-    print(strLine)
-
 print(len(shortest_path) - 1)
-print(shortest_path)
+
+# part b
+startCoord = []
+for row in range(len(elevation_map)):
+    for col in range(len(elevation_map[row])):
+        if elevation_map[row][col] == 'E':
+            startCoord = [row, col]
+shortest_path = []
+already_visited = [startCoord]
+priority_queue = [[startCoord, [startCoord]]]
+
+while(len(priority_queue) > 0):
+    curr_node = priority_queue.pop(0)
+    row = curr_node[0][0]; col = curr_node[0][1]
+    curr_history = curr_node[1].copy()
+    for dr, dc in [[0,1],[1,0],[0,-1],[-1,0]]:
+        r = row + dr; c = col + dc
+        if not (0 <= r < len(elevation_map) and 0 <= c < len(elevation_map[0])):
+            continue
+        if not (letter_to_number[elevation_map[r][c]] >= letter_to_number[elevation_map[row][col]] - 1):
+            continue
+        if [r, c] in already_visited:
+            continue
+        already_visited.append([r,c])
+        new_history = curr_history.copy()
+        new_history.append([r,c])
+        priority_queue.append([[r, c], new_history])
+        if elevation_map[r][c] == 'a':
+            shortest_path = new_history
+            priority_queue.clear()
+            break
+print(len(shortest_path) - 1)
